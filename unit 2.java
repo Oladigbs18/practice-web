491bdf1c2e7be8bcf1b2062e3fe33e239cb1a27d
Aaliyah <aaliyaholadigbolu@gmail.com>
2023-11-29 14:17:35 +0100

    my practice programming😍🎉✔

---
+++ unit 2.java
@@ -0,0 +1,34 @@
+// example of if-else statement
+int a = 10;
+int b = 20;
+if (a > b) {
+    System.out.println("a is greater than b");
+} else {
+    System.out.println("a is less than or equal to b");
+}
+
+// example using switch statement
+int day = 5;
+switch (day) {
+    case 1:
+        System.out.println("Monday");
+        break;
+    case 2:
+        System.out.println("Tuesday");
+        break;
+    case 3:
+        System.out.println("Wednesday");
+        break;
+    case 4:
+        System.out.println("Thursday");
+        break;
+    case 5:
+        System.out.println("Friday");
+        break;
+    case 6:
+        System.out.println("Saturday");
+        break;
+    case 7:
+        System.out.println("Sunday");
+        break;
+}
